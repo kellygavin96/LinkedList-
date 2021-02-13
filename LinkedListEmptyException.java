3f05f3fc1d17555152d34d51e3421d51fef0eb6a
kellygavin96 <76715132+kellygavin96@users.noreply.github.com>
2021-02-13 14:06:55 -0500

    Create LinkedListEmptyException.java

---
+++ LinkedListEmptyException.java
@@ -0,0 +1,11 @@
+//kelly briceno 
+
+class LinkedListEmptyException extends RuntimeException{
+       public LinkedListEmptyException(){
+         super();
+       }
+      
+     public LinkedListEmptyException(String message){
+         super(message);
+       }  
+}
